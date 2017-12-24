82dba0c5f5dadb5653b5160d1e8377f42cc23a44
Yadong Jiang <yadong.jiang@fe.up.pt>
2017-12-24 21:47:45 +0000

    mac

--- Harmony_Search.py
+++ Harmony_Search.py
@@ -24,16 +24,56 @@ class Harmony_Search():
         
         self.generate_HM()
 
+    def bool_probability(self, probability):
+        return (np.random.random() < probability)
+
     def random_pitch(self, pit_range):
         return np.random.uniform(pit_range[0], pit_range[1])
 
+    def random_harmony_vector(self):
+        return np.array([self.random_pitch(self.pit_range)])
+
+    def random_pit_from_list(self, target_list):
+        ind = int(len(target_list) * np.random.random());
+        return target_list[ind]
+
+    def new_harmony_vector(self):
+        if self.bool_probability(self.hmcr):
+            new_pit = np.array([self.random_pit_from_list(self.HM[:,0])])
+            if self.bool_probability(self.par):
+                new_pit = new_pit + self.fw * np.random.uniform(-1.0, 1.0)
+        else:
+            new_pit = self.random_pitch(self.pit_range)
+
+        return np.array([new_pit])
+
     def generate_HM(self):
-        self.HM = np.zeros((self.hms,1))
         self.HM_f = np.zeros(self.hms)
         for i in range(self.hms):
-            self.HM[i,0] = self.random_pitch(self.pit_range)
+            if i == 0:
+                self.HM = self.random_harmony_vector()
+            else:
+                self.HM = np.vstack((self.HM, self.random_harmony_vector()))
             self.HM_f[i] = self.obj_fun(self.HM[i])
         return 0
 
-    def bool_probability(self, probability):
-        return (np.random.random() < probability)
+    def update_HM(self, harmony_vector):
+        new_vector = harmony_vector
+        new_f = self.obj_fun(new_vector)
+        ind_max = self.HM_f.argmax()
+        HM_f_max = self.HM_f[ind_max]
+        if new_f < HM_f_max:
+            self.HM_f[ind_max] = new_f
+            self.HM[ind_max] = new_vector
+
+    def Optimized(self):
+        ind_min = self.HM_f.argmin()
+        HM_f_min = self.HM_f[ind_min]
+        HM_min = self.HM[ind_min]
+        return HM_min, HM_f_min
+
+    def Optimization(self):
+        for i in range(self.max_iter):
+            self.update_HM(self.new_harmony_vector())
+            print(self.Optimized())
+            
--- test.py
+++ test.py
@@ -3,13 +3,13 @@ from Harmony_Search import Harmony_Search
 
 pit_range = (0.0, 9.0)
 hms = 10
-fw = 0.5
+fw = 0.1
 tol = 1.0e-05 
-max_iter = 100
+max_iter = 100000
 
 def obj_fun(x):
-    return x - np.pi
+    return np.abs(x - np.pi)
 
 HS = Harmony_Search(pit_range, hms, fw, obj_fun, tol, max_iter, hmcr=0.9, par=0.3)
-print(HS.bool_probability(0.5))
-
+HS.Optimization()
+HS.Optimized()
