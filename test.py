dc1cacfeabf266911853565839f268de0c81813c
Yadong Jiang <yadong.jiang@fe.up.pt>
2017-12-25 15:52:37 +0000

    mac

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import numpy as np
 from Harmony_Search import Harmony_Search
 
-ins_num = 4
+ins_num = 3
 pit_range = np.array([
-    [0.0, 100.0],
     [0.0, 100.0],
     [0.0, 100.0],
     [0.0, 100.0],
@@ -16,8 +15,8 @@ tol = 1.0e-03
 max_iter = 500000
 
 def obj_fun(vector):
-    x, y, z, k = vector
-    f = (x-3.0)**2 + (y-6.0)**2 + (z-88.0)**2 + (k - 50.0)**2
+    x, y, z = vector
+    f = (x-3.0)**2 + (y-6.0)**2 + (z-88.0)**2
     return f
 
 HS = Harmony_Search(ins_num, pit_range, hms, obj_fun, tol, max_iter)
