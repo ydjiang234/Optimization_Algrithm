77366080bdfa0fe15a693195f13cccae3b45bef5
Yadong Jiang <yadong.jiang@fe.up.pt>
2017-12-25 15:47:29 +0000

    mac

--- Harmony_Search.py
+++ Harmony_Search.py
@@ -12,39 +12,39 @@ class Harmony_Search():
     #tol: Tolerance
     #max_iter: Maximum Iteration Number - an int
 
-    def __init__(self, ins_num, pit_range, hms, fw, obj_fun, tol, max_iter, hmcr=0.9, par=0.3):
+    def __init__(self, ins_num, pit_range, hms, obj_fun, tol, max_iter, hmcr=0.9, par=0.3, fw_ratio=0.01):
         self.ins_num = ins_num
         self.pit_range = pit_range
         self.hms = hms
-        self.fw = fw
+        self.fw_ratio = fw_ratio
         self.obj_fun = obj_fun
         self.tol = tol
         self.max_iter = max_iter
         self.hmcr = hmcr
         self.par = par
-        
+       
+        self.covert_default()
         self.generate_HM()
+
+    def covert_default(self):
+        if type(self.hmcr) != type(np.array([1,2])):
+            self.hmcr = np.ones(self.ins_num) * self.hmcr
+        if type(self.par) != type(np.array([1,2])):
+            self.par = np.ones(self.ins_num) * self.par
+        self.fw = self.fw_ratio * (self.pit_range[:,1] - self.pit_range[:,0])
+
 
     def bool_probability(self, probability):
         return (np.random.random() < probability)
-
-
-    def random_harmony_vector(self):
-        for i in range(self.ins_num):
-            if i==0:
-                new_vector = np.array(self.random_pitch(self.pit_range[i]))
-            else:
-                new_vector = np.append(new_vector, self.random_pitch(self.pit_range[i]))
-        return new_vector
 
     def random_pit_from_list(self, target_list):
         ind = int(len(target_list) * np.random.random());
         return target_list[ind]
 
     def select_pitch(self, ins_ind):
-        if self.bool_probability(self.hmcr):
+        if self.bool_probability(self.hmcr[ins_ind]):
             new_pit = self.random_pit_from_list(self.HM[:,ins_ind])
-            if self.bool_probability(self.par):
+            if self.bool_probability(self.par[ins_ind]):
                 new_pit = new_pit + self.fw[ins_ind] * np.random.uniform(-1.0, 1.0)
         else:
             new_pit = np.random.uniform(self.pit_range[ins_ind,0], self.pit_range[ins_ind,1])
@@ -52,16 +52,16 @@ class Harmony_Search():
 
     def new_harmony_vector(self):
         new_vector = np.zeros(self.ins_num)
-        for i in range(self.ins_num):
-            new_vector[i] = self.select_pitch(i)
+        for ins_ind in range(self.ins_num):
+            new_vector[ins_ind] = self.select_pitch(ins_ind)
         return new_vector
 
     def generate_HM(self):
         self.HM = np.zeros((self.hms, self.ins_num))
         self.HM_f = np.zeros(self.hms)
         for i in range(self.hms):
-            for j in range(self.ins_num):
-                self.HM[i, j] = np.random.uniform(self.pit_range[j,0], self.pit_range[j,1])
+            for ins_ind in range(self.ins_num):
+                self.HM[i, ins_ind] = np.random.uniform(self.pit_range[ins_ind,0], self.pit_range[ins_ind,1])
             self.HM_f[i] = self.obj_fun(self.HM[i])
         return 0
 
@@ -83,5 +83,8 @@ class Harmony_Search():
     def Optimization(self):
         for i in range(self.max_iter):
             self.update_HM(self.new_harmony_vector())
-            print(self.Optimized())
+            HM_min, HM_f_min = self.Optimized()
+            print(HM_min, HM_f_min)
+            if HM_f_min <= self.tol:
+                break
             
--- test.py
+++ test.py
@@ -9,8 +9,10 @@ pit_range = np.array([
     [0.0, 100.0],
     ])
 hms = 10000
-fw = np.array([1.0, 1.0, 1.0, 1.0])
-tol = 1.0e-05 
+#fw_ratio = np.array([0.01, 0.01, 0.01, 0.01])
+#hmcr = np.array([0.9, 0.9, 0.9, 0.9])
+#par = np.array([0.1, 0.1, 0.1, 0.1])
+tol = 1.0e-03 
 max_iter = 500000
 
 def obj_fun(vector):
@@ -18,6 +20,6 @@ def obj_fun(vector):
     f = (x-3.0)**2 + (y-6.0)**2 + (z-88.0)**2 + (k - 50.0)**2
     return f
 
-HS = Harmony_Search(ins_num, pit_range, hms, fw, obj_fun, tol, max_iter, hmcr=0.9, par=0.3)
+HS = Harmony_Search(ins_num, pit_range, hms, obj_fun, tol, max_iter)
 HS.Optimization()
 HS.Optimized()
