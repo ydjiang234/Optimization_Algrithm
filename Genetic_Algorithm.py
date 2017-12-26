f7cda102748120709e30dbd73c5fe9f0cc02286e
Yadong Jiang <yadong.jiang@fe.up.pt>
2017-12-26 22:46:46 +0000

    mac

--- Genetic_Algorithm.py
+++ Genetic_Algorithm.py
@@ -1,6 +1,47 @@
 import numpy as np
+import random
 
 class chromosome():
+
+    def __init__(self, bin_str, bin_limit):
+        self.bin_str = bin_str
+        self.bin_limit = bin_limit
+        self.bin_len = len(bin_str)
+
+    def bool_probability(self, probability):
+        return (np.random.random() < probability)
+
+    def bool_outrange(self, bin_str):
+        return int(bin_str, 2) > int(self.bin_limit, 2)
+
+    def random_int_list(self, lower, upper, num):
+        return random.sample(range(lower, upper), num)
+
+    def change_str_letter(self, string, ind, target):
+        temp = list(string)
+        temp[ind] = target
+        return ''.join(temp)
+
+    def mutate_single(self, ind):
+        string = self.bin_str[ind]
+        if string=='0':
+            out = self.change_str_letter(self.bin_str, ind, '1')
+        elif string=='1':
+            out = self.change_str_letter(self.bin_str, ind, '0')
+        if not self.bool_outrange(out):
+            self.bin_str = out
+
+    def mutate(self, prob, num=1):
+        if self.bool_probability(prob):
+            ind_mut = self.random_int_list(0, self.bin_len, num)
+            for ind in ind_mut:
+                self.mutate_single(ind)
+
+class Genetic_Algorithm():
+
+    def __init__(self, var_num, var_range, var_digit, population, obj_fun, mutation_prop=0.1):
+
+class covertor():
 
     def __init__(self, dec, dec_range, dec_digit):
         self.dec = dec;
--- test_GA.py
+++ test_GA.py
@@ -1,8 +1,24 @@
 import numpy as np
-from Genetic_Algorithm import chromosome
+from Genetic_Algorithm import chromosome, Genetic_Algorithm
 
-dec = 50.0
-dec_range = np.array([-13.0, 108.0])
-dec_digit = 1000
-chrom = chromosome(dec, dec_range, dec_digit)
-chrom.mutate(0.5, 5)
+var_num = 2
+var_range = np.array([
+    [0.0, 100.0],
+    [0.0, 100.0],
+    ])
+var_digit = [1000, 1000]
+population = 10
+mutation_prop = 0.1
+
+#fw_ratio = np.array([0.01, 0.01, 0.01, 0.01])
+#hmcr = np.array([0.9, 0.9, 0.9, 0.9])
+#par = np.array([0.1, 0.1, 0.1, 0.1])
+tol = 1.0e-03 
+max_iter = 500
+
+def obj_fun(vector):
+    x, y = vector
+    f = (x-3.0)**2 + (y-6.0)**2
+    return f
+
+GA = Genetic_Algorithm(var_num, var_range, var_digit, population, obj_fun)
