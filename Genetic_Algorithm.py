1c50fc45664e2885358128aa72ed37822c370f9d
Yadong Jiang <yadong.jiang@fe.up.pt>
2017-12-30 23:42:28 +0000

    MAC

--- Genetic_Algorithm.py
+++ Genetic_Algorithm.py
@@ -1,6 +1,116 @@
 import numpy as np
+import copy
 import random
 
+class chromosome():
+
+    def __init__(self, vector, var_range, var_digit):
+        self.vector = vector
+        self.var_num = self.vector.size
+        self.var_range = var_range
+        self.var_digit = var_digit
+        self.update_gene_info()
+        self.encode()
+
+    def copy(self):
+        new_chrom = chromosome(self.vector, self.var_range, self.var_digit)
+        return new_chrom
+
+    def update_gene_info(self):
+        self.int_range = np.zeros((self.var_num, 2), dtype=int)
+        self.bin_limit = list([])
+        self.bin_len = np.zeros(self.var_num, dtype=int)
+        for i in range(self.var_num):
+           temp = (self.var_range[i] - self.var_range[i,0]) * self.var_digit[i]
+           self.int_range[i] = temp.astype(int)
+           self.bin_limit.append('{0:b}'.format(self.int_range[i,1]))
+           self.bin_len[i] = len(self.bin_limit[i])
+        self.bin_limit = np.array(self.bin_limit)
+
+    def random_int_list(self, lower, upper, num):
+        out = random.sample(range(lower, upper), num)
+        out.sort()
+        return out
+
+    def dec2bin(self, var, var_ind):
+        out = '{0:b}'.format(int((var - self.var_range[var_ind,0]) * self.var_digit[var_ind]))
+        return out.zfill(self.bin_len[var_ind])
+
+    def bin2dec(self, var_bin, var_ind):
+        out = int(var_bin, 2)
+        return out/self.var_digit[var_ind] + self.var_range[var_ind,0]
+    
+    def encode(self):
+        self.gene_group = list([])
+        for var_ind in range(self.var_num):
+            temp_str = self.dec2bin(self.vector[var_ind], var_ind)
+            temp_gene = gene(temp_str, self.bin_limit[var_ind])
+            self.gene_group.append(temp_gene)
+
+    def decode(self):
+        for var_ind in range(self.var_num):
+            temp_gene = self.gene_group[var_ind]
+            self.vector[var_ind] = self.bin2dec(temp_gene.bin_str, var_ind)
+
+    def crossover(self, chrom1, chrom2, break_num):
+        break_points = self.random_int_list(1, chrom1.var_num, break_num)
+        new_chrom1 = chrom1.copy()
+        new_chrom2 = chrom2.copy()
+        i = 0
+        while i < (len(break_points) - 1):
+            for j in range(break_points[i], break_points[i+1]):
+                new_chrom1.gene_group[j] = chrom2.gene_group[j]
+                new_chrom2.gene_group[j] = chrom1.gene_group[j]
+            i += 2
+        if i == (len(break_points) - 1):
+            for j in range(break_points[i], self.var_num):
+                new_chrom1.gene_group[j] = chrom2.gene_group[j]
+                new_chrom2.gene_group[j] = chrom1.gene_group[j]
+        new_chrom1.decode()
+        new_chrom2.decode()
+        return new_chrom1, new_chrom2
+
+class gene():
+
+    def __init__(self, bin_str, bin_limit):
+        self.bin_str = bin_str
+        self.bin_limit = bin_limit
+        self.bin_len = len(bin_str)
+
+    def bool_probability(self, probability):
+        return (np.random.random() < probability)
+
+    def bool_outrange(self, bin_str):
+        return int(bin_str, 2) > int(self.bin_limit, 2)
+
+    def random_int_list(self, lower, upper, num):
+        out = random.sample(range(lower, upper), num)
+        out.sort()
+        return out
+
+    def change_str_letter(self, string, ind, target):
+        temp = list(string)
+        temp[ind] = target
+        return ''.join(temp)
+
+    def mutate_single(self, ind):
+        string = self.bin_str[ind]
+        if string=='0':
+            out = self.change_str_letter(self.bin_str, ind, '1')
+        elif string=='1':
+            out = self.change_str_letter(self.bin_str, ind, '0')
+        if not self.bool_outrange(out):
+            self.bin_str = out
+
+    def mutate(self, prob, num=1):
+        num = np.random.randint(low=0, high=self.bin_len)
+        if self.bool_probability(prob):
+            ind_mut = self.random_int_list(0, self.bin_len, num)
+            for ind in ind_mut:
+                self.mutate_single(ind)
+
+
+'''
 class chromosome():
 
     def __init__(self, bin_str):
@@ -31,9 +141,10 @@ class chromosome():
         elif string=='1':
             out = self.change_str_letter(self.bin_str, ind, '0')
         #if not self.bool_outrange(out):
-            #self.bin_str = out
+        self.bin_str = out
 
-    def mutate(self, prob, num=1):
+    def mutate(self, prob):
+        num = np.random.randint(low=0, high=self.bin_len)
         if self.bool_probability(prob):
             ind_mut = self.random_int_list(0, self.bin_len, num)
             for ind in ind_mut:
@@ -66,9 +177,7 @@ class chromosome():
         new_str1 = ''.join(new_str1)
         new_str2 = ''.join(new_str2)
         return chromosome(new_str1), chromosome(new_str2)
-
-
-
+'''
 
 class Genetic_Algorithm():
 
@@ -104,7 +213,7 @@ class Genetic_Algorithm():
 
     def update_sel_prop(self):
         sort_ind = self.fitness.argsort() + 1.0
-        self.sel_prop = sort_ind / np.sum(sort_ind)
+        self.sel_prop = sort_ind / len(sort_ind)
 
     def add(self, chrom):
         self.group.append(chrom)
@@ -112,9 +221,13 @@ class Genetic_Algorithm():
         self.fitness = np.append(self.fitness, self.obj_fun(temp))
         self.update_sel_prop()
 
-    def delete(self, ind):
-        del self.group[ind]
-        self.fitness = np.delete(self.fitness, ind)
+    def delete(self, ind_list):
+        temp_group = self.group
+        self.group = list([])
+        self.fitness = np.array([])
+        for i in range(len(temp_group)):
+            if i not in ind_list:
+                self.add(temp_group[i])
         self.update_sel_prop()
 
     def select(self):
@@ -143,10 +256,7 @@ class Genetic_Algorithm():
             cur_ind += 1
             if cur_ind == len(self.group):
                 cur_ind = 0
-
-        for i in del_ind:
-            self.delete(i)
-        
+        self.delete(del_ind)
 
 
     def cross(self, cross_ind):
@@ -166,6 +276,73 @@ class Genetic_Algorithm():
         self.eliminate()
 
 
+'''
+class chromosome():
+
+    def __init__(self, bin_str):
+        self.bin_str = bin_str
+        #self.bin_limit = bin_limit
+        self.bin_len = len(bin_str)
+
+    def bool_probability(self, probability):
+        return (np.random.random() < probability)
+
+    #def bool_outrange(self, bin_str):
+        #return int(bin_str, 2) > int(self.bin_limit, 2)
+
+    def random_int_list(self, lower, upper, num):
+        out = random.sample(range(lower, upper), num)
+        out.sort()
+        return out
+
+    def change_str_letter(self, string, ind, target):
+        temp = list(string)
+        temp[ind] = target
+        return ''.join(temp)
+
+    def mutate_single(self, ind):
+        string = self.bin_str[ind]
+        if string=='0':
+            out = self.change_str_letter(self.bin_str, ind, '1')
+        elif string=='1':
+            out = self.change_str_letter(self.bin_str, ind, '0')
+        #if not self.bool_outrange(out):
+        self.bin_str = out
+
+    def mutate(self, prob):
+        num = np.random.randint(low=0, high=self.bin_len)
+        if self.bool_probability(prob):
+            ind_mut = self.random_int_list(0, self.bin_len, num)
+            for ind in ind_mut:
+                self.mutate_single(ind)
+
+    def division(self, break_points):
+        out = list([])
+        for i in range(len(break_points)+1):
+            if i==0:
+                out.append(self.bin_str[:break_points[i]])
+            elif i == len(break_points):
+                out.append(self.bin_str[break_points[i-1]:])
+            else:
+                out.append(self.bin_str[break_points[i-1]: break_points[i]])
+        return np.array(out)
+
+    def crossover(self, chrom1, chrom2, break_num):
+        break_points = self.random_int_list(1, chrom1.bin_len, break_num)
+        parts1 = chrom1.division(break_points)
+        parts2 = chrom2.division(break_points)
+        new_str1 = list([]) 
+        new_str2 = list([]) 
+        for i in range(len(parts1)):
+            if i%2 == 0:
+                new_str1.append(parts1[i])
+                new_str2.append(parts2[i])
+            else:
+                new_str1.append(parts2[i])
+                new_str2.append(parts1[i])
+        new_str1 = ''.join(new_str1)
+        new_str2 = ''.join(new_str2)
+        return chromosome(new_str1), chromosome(new_str2)
 
 class coder():
 
@@ -208,4 +385,4 @@ class coder():
             temp_bin = bin_str[temp1:temp2]
             out[var_ind] = self.bin2dec(temp_bin, var_ind)
         return out
-
+'''
--- test_GA.py
+++ test_GA.py
@@ -1,33 +1,48 @@
 import numpy as np
-from Genetic_Algorithm import chromosome, Genetic_Algorithm, coder
+from Genetic_Algorithm import chromosome, Genetic_Algorithm
 
-var_num = 3
+var_num = 4
 var_range = np.array([
     [0.0, 100.0],
     [0.0, 100.0],
     [0.0, 100.0],
+    [0.0, 100.0],
     ])
-var_digit = [1000, 1000, 10]
-population = 10
+var_digit = [1000, 1000, 1000, 1000]
+population = 100
 mutation_prop = 0.1
 var_num, var_range, var_digit
 #fw_ratio = np.array([0.01, 0.01, 0.01, 0.01])
 #hmcr = np.array([0.9, 0.9, 0.9, 0.9])
 #par = np.array([0.1, 0.1, 0.1, 0.1])
 tol = 1.0e-03 
-max_iter = 500
+max_iter = 10000
 
 def obj_fun(vector):
-    x, y, z = vector
-    f = (x-3.0)**2 + (y-6.0)**2 + (z-50.0)**2
-    return f
+    x, y, m, n = vector
+    f = (x-3.0)**2 + (y-20.0)**2 + (m-98.0)**2 + (n-23.0)**2
+    return 1.0/f
+'''
+GA = Genetic_Algorithm(var_num, var_range, var_digit, population, obj_fun, cross_num=3, sel_por=0.3, mutation_prop=0.1)
+fitness = np.array([])
+vector = np.zeros((1,var_num))
+for i in range(max_iter):
+    GA.evolve()
+    ind = GA.fitness.argmax()
+    fitness = np.append(fitness, GA.fitness[ind])
+    vector = np.vstack((vector, GA.cd.decode(GA.group[ind].bin_str)))
+    if i == 0:
+        vector = vector[1:]
+    #print(GA.fitness)
+    if max(GA.fitness) > 1.0e3:
+        print(GA.cd.decode(GA.group[ind].bin_str))
+        break
 
-GA = Genetic_Algorithm(var_num, var_range, var_digit, population, obj_fun)
-
-a = coder(var_num, var_range, var_digit)
-b = a.encode(np.array([10.0, 20.0, 98.0]))
-c = a.encode(np.array([4.0, 15.0, 80.0]))
-m = chromosome(b)
-n = chromosome(c)
-GA.evolve()
-print(len(GA.group))
+ind = GA.fitness.argmax()
+print(vector[ind])
+'''
+a = chromosome(np.array([12.0, 15.0,4.0,90.0]),var_range, var_digit)
+b = chromosome(np.array([80.0, 2.0, 54.0,27.0]),var_range, var_digit)
+m, n = a.crossover(a,b,2)
+print(a.vector, b.vector)
+print(m.vector, n.vector)
